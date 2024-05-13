01f566a3a88af0dd0963da3780aa0abb7ed69fa4
scs0209 <tjdckdtn2463@naver.com>
2024-05-13 22:14:28 +0900

    feat: 3d 웹사이트 초기 셋팅 완료

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(mov|mp4)$/,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+          },
+        },
+      ],
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
