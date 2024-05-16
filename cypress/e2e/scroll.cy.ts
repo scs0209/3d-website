1e693da06766bcc558f58f9dfad29ec4db23ff86
scs0209 <tjdckdtn2463@naver.com>
2024-05-16 23:44:26 +0900

    fix: jest와 cypress formatter conflict 해결

--- cypress/e2e/scroll.cy.ts
+++ cypress/e2e/scroll.cy.ts
@@ -1,5 +1,13 @@
-describe("template spec", () => {
-  it("passes", () => {
+describe("Hero Component", () => {
+  beforeEach(() => {
     cy.visit("/");
+  });
+
+  it("loads smallHeroVideo on small screens", () => {
+    cy.viewport(500, 800); // 작은 화면 크기로 설정합니다.
+    cy.get("video").then(($video) => {
+      const src = $video.find("source").attr("src");
+      expect(src).to.contain("smallHeroVideo");
+    });
   });
 });
